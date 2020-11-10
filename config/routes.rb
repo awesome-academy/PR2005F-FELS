c47236f35cfee35070df792c52a60a82e3ee35d2
concunmun98 <mstaion508@gmail.com>
2020-11-10 14:02:41 +0700

    User_Devise_Boostrap

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,16 @@ Rails.application.routes.draw do
   get 'pages/research'
   get 'pages/course'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   devise_for :users,
+            path: '',
+            path_names: {sign_in: 'login' ,sign_out: 'logout' ,edit: 'profile',sign_up: 'resgistration'},
+            controllers: {omniauth_callbacks: 'omniauth_callbacks' }
+
+      as :user do
+        get "signin" => "devise/sessions#new"
+        post "signin" => "devise/sessions#create"
+      devise_scope :user do
+        get '/users/sign_out' => 'devise/sessions#destroy'
+      end
+  end
 end
